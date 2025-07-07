4732ae2272a8eeb576ccd7a08da1d9319f5cb39d
Mohammad7871 <alex667871>
2025-07-08 01:18:47 +0500

    it hello world ,

---
+++ Hellow.C
@@ -0,0 +1,32 @@
+// #include <stdio.h>
+// #include   it is preprocessor directive
+// #include <stdio.h> is a preprocessor directive that includes the standard input/output library
+// It allows us to use functions like printf and scanf
+
+
+//std io.h   --->  this is header file which contains the declaration of printf and scanf functions
+// stdio.h is a standard header file in C that provides functionalities for input and output operations.
+// It includes functions like printf for printing output to the console and scanf for reading input from the console.
+
+// int main(){
+
+//     return 0; // This line indicates that the program has executed successfully and returns control to the operating system.
+// }
+
+
+// int means it is interger value out put 
+// main is the entry point of the program, where execution starts.,, compiler seek for it to start thr running of the program
+// insted of int we can use void also which means it is function with zero output
+
+
+
+#include <stdio.h> 
+
+int main (){
+    printf("Hello Dear world");
+    return 0;
+}
+
+
+
+
