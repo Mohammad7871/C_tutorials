c6992a2c98df70531642e18befd93c2ca9bd6bdd
Mohammad7871 <alex667871>
2025-07-09 10:36:14 +0500

    variable in C and there use

---
+++ variable.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    printf("/n /n starting our next work from here /n we will discussabout the variable types in C++ /n");
+    printf("There are several types of variables in C++: \n");
+    printf("1. int - for integers \n");
+    printf("2. float - for floating point numbers \n");
+    printf("3. double - for double precision floating point numbers \n");
+    printf("4. char - for characters \n");
+    printf("5. bool - for boolean values (true/false) \n");
+    
+    printf("6. string - for strings of characters \n");
+    printf("7. array - for collections of elements of the same type \n");
+    printf("8. struct - for user-defined data types that group different types together \n");
+    printf("9. enum - for enumerated types, which define a variable that can hold a set of predefined constants \n");
+    printf("10. pointer - for variables that store memory addresses of other variables \n");
+    printf("11. reference - for variables that are aliases for other variables \n");
+    printf("12. union - for user-defined data types that can hold different types in the same memory location \n");
+    printf("13. auto - for automatic type deduction (C++11 and later) \n");
+    printf("14. nullptr - for null pointer (C++11 and later) \n");
+    
+    
+    
+    return 0;
+}
